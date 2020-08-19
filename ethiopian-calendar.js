71ccfd42a1eecf755169aba9e14b17c00da9d481
Mayank Gautam <mgautam099@gmail.com>
2020-08-19 19:57:07 +0530

    test

--- ethiopian-calendar.js
+++ ethiopian-calendar.js
@@ -98,7 +98,7 @@ class EthiopianCalendar extends Widget {
         // this._showSelected( template.querySelector( '.selected' ) );
         // template.addEventListener('change', this._change.bind(this))
         // this._addOnChangeListener(template)
-        return $template.querySelector('.selectpicker');
+        return $template.find('.selectpicker');
     }
 
     _change( ev ) {
